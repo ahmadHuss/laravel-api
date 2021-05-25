132769b8091499eb86984b495fc7b726feb8a88f
ahmadhuss <ahmadnet600@gmail.com>
2021-05-25 16:30:12 +0500

    refactor: Getting Single Category with only id & name

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceResponse;
@@ -17,13 +18,14 @@ class CategoryController extends Controller
 
     // Route model binding
     public function show(Category $category){
-        return $category; // It will return category as a JSON response object and it is the magic of Laravel
+        // return $category; // It will return category as a JSON response object and it is the magic of Laravel
 
         // It is difficult to return only id and name with the Route model binding
         // There are only 2 ways:
         // 1.Either you can use collection methods to filter
         // 2.But now  it is time to introduce API resources which will contain set of rules for the API.
         // php artisan make:resource CategoryResource
+        return new CategoryResource($category);
     }
 
 
--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -14,6 +14,10 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // In this method we have to override the results.
+        return [
+          'id' => $this->id,
+          'name' => $this->name
+        ];
     }
 }
