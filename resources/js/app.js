b2f8363d6102ff97ad4a91b0a93099b26b5c32e4
ahmadhuss <ahmadnet600@gmail.com>
2021-06-01 22:11:06 +0500

    feat: Added Vue.js component named `Front.vue`

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+// Require Vue
+window.Vue = require('vue');
+
+// id app
+Vue.component('front-page', require('./components/Front.vue').default);
+
+// load the front component
+const app = new Vue({
+    el: '#app'
+});
--- resources/js/components/Front.vue
+++ resources/js/components/Front.vue
@@ -9,13 +9,6 @@
             <div class="col-lg-3">
 
                 <h1 class="my-4">Shop Catalog</h1>
-                @if(count($categories) > 0)
-                <div class="list-group">
-                    @foreach($categories as $category)
-                    <a href="/?category={{ $category->id }}" class="list-group-item">{{ $category->name }}</a>
-                    @endforeach
-                </div>
-                @endif
 
             </div>
             <!-- /.col-lg-3 -->
@@ -23,28 +16,6 @@
             <div class="col-lg-9">
 
                 <div class="row my-5">
-
-                    @if(count($categories) > 0)
-                    @foreach($products as $product)
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card h-100">
-                            <a href="/product/{{ $product->name }}">
-                                <img class="card-img-top" src="http://placehold.it/700x400" alt="">
-                            </a>
-                            <div class="card-body">
-                                <h4 class="card-title">
-                                    <a href="/product/{{ $product->name }}">{{ ucfirst($product->name) }}</a>
-                                </h4>
-                                <h5>${{ $product->price }}</h5>
-                                <p class="card-text">
-                                    {{ $product->description }}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
-                    @endif
-
 
                 </div>
                 <!-- /.row -->
@@ -57,6 +28,10 @@
 
     </div>
 </template>
+
+
+<!-- JavaScript Part -->
+<script></script>
 
 
 
