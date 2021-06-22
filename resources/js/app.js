50bf3777b1c7a3138fc662f5ce5767e8daec5d1f
ahmadhussnain <ahmad.hussnain@eliteengineering.com.pk>
2021-06-22 13:33:08 +0500

    webpack.mix.js config has been updated
    * Update compile js file immediately.
    * Update compile css file immediately.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,5 @@
+require('./bootstrap');
 import Vue from 'vue';
-import axios from 'axios';
-import _ from 'lodash';
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 
 window.Vue = Vue;
 // Do `Vue.prototype._` = _ to use lodash as _ in templates. But I recommend you to define
